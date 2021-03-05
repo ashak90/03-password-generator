d10804c45b74dcb46821c7c3ad87aa870b22f209
Asha Kumar <ashakumar90@gmail.com>
2021-03-04 22:22:22 -0500

    fix the for loop that produces the finalPass

--- script.js
+++ script.js
@@ -33,9 +33,9 @@ function generatePassword () {
     allowedCharac = allowedCharac + numbers;
   }
 
-  for (var i=0; i<=passwordLength; i++){
-    var randomNumber = Math.floor(Math.random() * Math.floor(allowedCharac.length-1));
-    finalPass += allowedCharac.substring(randomNumber, randomNumber+1);
+  for (var i=0; i<passwordLength; i++){
+    var randomNumber = Math.floor(Math.random() * allowedCharac.length);
+    finalPass += allowedCharac[randomNumber];
   }
 
   return finalPass;
